6304c4680ae154ef12a2c008ff2cadb8799306a9
Linuxfun51 <91023363+EgeresiMihaly@users.noreply.github.com>
2023-06-24 12:26:24 +0200

    add movie list py

---
+++ py_components/movie_list.py
@@ -0,0 +1,22 @@
+from typing import Optional, Union
+from PySide6.QtCore import QAbstractListModel, Qt , QModelIndex
+
+class MovieList(QAbstractListModel):
+    DataRole = Qt.UserRole
+
+    def __init__(self): 
+        super().__init__()
+        self._movies = []
+
+    def rowCount(self, parent= QModelIndex) -> int:
+        return len(self._moves)
+    
+    def roleNames(slef):
+        return{
+            MovieList.DataRole: b'movie_data'
+        }
+
+    def data(self, index, role=Qt.DisplayRole):
+        row = index.row()
+        if role == MovieList.DataRole:
+            return self._movies[row]
